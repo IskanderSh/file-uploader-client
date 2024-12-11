99ca2c3ca869cbc2663adfd1f89333b224508eb3
IskanderSh <is2367361@gmail.com>
2024-12-11 23:41:18 +0300

    init some code

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/IskanderSh/tages-task"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:1111", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	client := pb.NewFileServiceClient(conn)
+
+	var fileName string
+	flag.StringVar(&fileName, "file-name", "test.txt", "file name")
+	flag.Parse()
+
+	_, err := os.Stat(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+}
