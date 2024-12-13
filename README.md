4960e707cff07405232e01933ca035830f8ac4d1
IskanderSh <is2367361@gmail.com>
2024-12-13 17:35:01 +0300

    add functions and readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# file-uploader-client
+# File uploader client
+
+## How to run:
+- Clone the repository: ```git clone https://github.com/IskanderSh/file-uploader-client.git```
+- Run the client (check instruction first): ```go run main.go --file={file} --func={func}```
+
+## Run instruction:
+There are 2 flags for running:
+- func: says which function you want run(upload, download, fetch)
+- file: if it is upload/download function, it will upload/download file with such name
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"io"
 	"os"
 
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
 
-	pb "github.com/IskanderSh/tages-task"
+	pb "github.com/IskanderSh/tages-task/proto"
 )
+
+var client pb.FileProviderClient
+var fileName string
 
 func main() {
 	conn, err := grpc.Dial("localhost:1111", grpc.WithInsecure())
@@ -16,19 +23,97 @@ func main() {
 	}
 	defer conn.Close()
 
-	pb.NewFileServiceClient(conn)
+	client = pb.NewFileProviderClient(conn)
 
-	var fileName string
-	flag.StringVar(&fileName, "file-name", "test.txt", "file name")
+	file := flag.String("file", "test.txt", "path to file")
+	function := flag.String("func", "fetch", "which function to call")
 	flag.Parse()
 
-	_, err = os.Stat(fileName)
+	fmt.Printf("running file-fetch-client with function: %s and using file: %s", *function, *file)
+	fileName = *file
+
+	switch *function {
+	case "upload":
+		upload()
+	case "download":
+		download()
+	case "fetch":
+		fetch()
+	}
+}
+
+func upload() {
+	_, err := os.Stat(fileName)
 	if err != nil {
 		panic(err)
 	}
 
-	_, err = os.Open(fileName)
+	file, err := os.Open(fileName)
 	if err != nil {
 		panic(err)
 	}
+	defer file.Close()
+
+	stream, err := client.UploadFile(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	buffer := make([]byte, 1024*1024) // 1mb
+	for {
+		n, err := file.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(string(buffer[:n]))
+
+		// Отправляем чанк серверу
+		err = stream.Send(&pb.UploadFileRequest{
+			FileName: file.Name(),
+			Content:  buffer[:n],
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Завершаем загрузку
+	resp, err := stream.CloseAndRecv()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Имя загруженного файла: %v", resp.FileName)
+}
+
+func download() {
+	stream, err := client.DownloadFile(context.Background(), &pb.DownloadFileRequest{FileName: fileName})
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		response, err := stream.Recv()
+		if err == io.EOF {
+			fmt.Println("finishing receiving chunks")
+			break
+		} else if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("received new chunk: %s", string(response.Content))
+	}
+}
+
+func fetch() {
+	response, err := client.FetchFiles(context.Background(), &emptypb.Empty{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("fetch response data: %+v", response.Data)
 }
